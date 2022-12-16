048d87858c2099074a6ab904b3860743df10937f
ofac <ofaczero@gmail.com>
2022-12-16 11:21:18 -0500

    feat: database remote

--- config/app.php
+++ config/app.php
@@ -10,9 +10,9 @@
     $js       = $public . '/js';
 
     // Config Data Base
-    $host    = '127.0.0.1';
+    $host    = 'containers-us-west-163.railway.app';
     $user    = 'root';
-    $passwd  = '';
-    $name_db = 'confecamaras';
+    $passwd  = 'IzdG88g4co0e240iyc26';
+    $name_db = 'railway';
 
 
